ba136eb39f3f71ede910de70cc4e73cb2e2787fd
Bais26 <baisyufan2004@gmail.com>
2025-03-12 03:47:07 +0700

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build', // Menentukan output folder ke public/build (direktori ini diharapkan ada di proyek Laravel)
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
